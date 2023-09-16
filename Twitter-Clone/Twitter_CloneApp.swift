851e842afc4d1d08781053ec630241b574bd07c0
bilal-dallali <dallali-bilal@hotmail.fr>
2023-09-16 22:03:21 +0200

    Initial Commit

---
+++ Twitter-Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter-Clone
+//
+//  Created by Bilal Dallali on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
